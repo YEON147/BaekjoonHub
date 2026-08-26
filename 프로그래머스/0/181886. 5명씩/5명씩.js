1f7aa4feee3c62d66b2b36ce163e36dfe1b63e56
YEON <gomsi3024@gmail.com>
2026-08-26 23:42:10 +0900

    [level 0] Title: 5명씩, Time: 0.11 ms, Memory: 44.3 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,3 +1,7 @@
 function solution(names) {
-    return names.filter((_, i) => i%5 ==0);
+    const ans = [];
+    for (i=0; i < names.length; i += 5){
+        ans.push(names[i])
+    }
+    return ans;
 }
