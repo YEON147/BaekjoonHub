bd7a715df132fcca20b2a77fa161df615ab60caa
YEON <gomsi3024@gmail.com>
2026-08-28 22:12:20 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.08 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,6 +1,3 @@
 function solution(num_list) {
-    var answer = 0;
-    return num_list.length >= 11 ? 
-        num_list.reduce((sum, a) => sum+a):
-        num_list.reduce((sum, a) => sum*a);
+    return num_list.reduce((sum, a)=> num_list.length >= 11 ? sum+a : sum*a)
 }
