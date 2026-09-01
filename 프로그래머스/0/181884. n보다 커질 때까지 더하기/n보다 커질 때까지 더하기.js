083ea9096976f90fa0f2efbc72af413d3223c046
YEON <gomsi3024@gmail.com>
2026-09-01 22:27:52 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.09 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,3 +1,3 @@
-function solution(numbers, n) {
-    return numbers.reduce((sum, a) => sum > n? sum : sum+a);
+function solution(numbers, n){
+    return numbers.reduce((sum, a) => sum > n ? sum : sum+a);
 }
